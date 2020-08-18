f12f798c1ff4c2485664a84f6003c0c97084fdef
godlovericea <1020097250@qq.com>
2020-08-18 14:28:39 +0800

    upload

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,11 @@ const mobileRoutes = [
         hidden: true
     },
     {
+        path: '/test',
+        component: () => import('@/views/home/test'),
+        hidden: true
+    },
+    {
         path: '/404',
         component: () => import('@/views/404'),
         hidden: true
---
+++ src/views/home/test.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <input type="file" @change="handleFileChange" id="file">
+        <button @click="haha">确认</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Redirect',
+    methods: {
+        haha () {
+            this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+        },
+        handleFileChange (e) {
+            console.log(e)
+        }
+    }
+}
+</script>
